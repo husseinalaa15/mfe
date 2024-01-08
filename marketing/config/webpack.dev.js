5b78c2592e4a75f0745c4d5afa42d39c4b9a48d2
Hussein Alaa <74919905+husseinalaa15@users.noreply.github.com>
2024-01-08 21:59:25 +0200

    try marketing v0.0.2

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,36 +1,30 @@
-const {merge} = require("webpack-merge");
-// to merge diffrent webpack versions 
-
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-// it will take an html tag of our project and inject different scripts into it 
-
-const ModuleFedrationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
-const package = require("../package.json");
-
-const commonConfig = require("./webpack.common")
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
-    mode:"development",
-    devServer:{
-        port:8081,
-        historyApiFallback: {
-            index:'index.html',
-
-        }
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html',
     },
-    plugins:[
-        new ModuleFedrationPlugin({
-            name:"marketing",
-            filename:"remoteEntry.js",
-            exposes:{
-                "./MarketingApp" : "./src/bootstrap.js"
-            },
-            shared:package.dependencies
-        }),
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
 
-        
-    ]
-}
-
-module.exports = merge(commonConfig,devConfig);
+module.exports = merge(commonConfig, devConfig);
