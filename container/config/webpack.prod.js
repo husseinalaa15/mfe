c67f1a3da772aaeb26083d3210e4acf4ef73ced3
Hussein Alaa <74919905+husseinalaa15@users.noreply.github.com>
2024-01-10 00:56:46 +0200

    container test 1

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
 const webpack = require("webpack");
-
+const path = require("path");
 const domain = process.env.PRODUCTION_DOMAIN
 
 const prodConfig = {
