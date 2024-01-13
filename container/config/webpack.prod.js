d99ca4fc229aff07ab476828bab20f2cfa1f11be
Hussein Alaa <74919905+husseinalaa15@users.noreply.github.com>
2024-01-13 23:08:22 +0200

    edit prod container

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -2,9 +2,7 @@ const {merge} = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
-const webpack = require("webpack");
 const path = require("path");
-const domain = process.env.PRODUCTION_DOMAIN
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const prodConfig = {
@@ -12,6 +10,8 @@ const prodConfig = {
     output:{
         filename:"[name].[contenthash].js",
         path: path.resolve(__dirname, '../dist/container'),
+        publicPath: '/',
+
     },
     plugins:[
         new ModuleFederationPlugin({
